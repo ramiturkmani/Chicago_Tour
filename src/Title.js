86e1bbedc00422cac4c62361ff2beb7007cf3051
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-19 14:14:18 -0500

    header component built

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>CHICAGOTOUR</h1>
+      <div className="Title-Subtitle">Chicago Places to Visit</div>
+    </div>
+  )
+}
+
+export default Title
