b2e196344b18260d19b70a0e0bf9be3956f128ef
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-19 23:59:11 -0500

    added PLACELIST.js

---
+++ src/ PLACESList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PLACESList() {
+  return (
+    <div>
+      <h1>Keep your eye on this place for future content...</h1>
+    </div>
+  )
+}
+
+export default PLACESList
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
+import PLACESList from './PLACESList';
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <img src={logo} />
+      <PLACESList />
     </div>
   );
 }
