8f2a2ffbedbc9979767a8431ac6b852508cadabd
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-23 20:55:10 -0500

    dynamic content and footer

--- src/PLACESList.js
+++ src/PLACESList.js
@@ -1,30 +1,25 @@
+// src/PLACESList.js
 import React from 'react';
 import PLACESSpace from './PLACESSpace';
 import './PLACESList.css';
+import data from './places-data.json'
 
 function PLACESList() {
+
+    const places = data.map(({ title, address, images, hours }) => {
+        return (
+            <PLACESSpace
+                name={title}
+                address={address}
+                image={images[0]}
+                hours={hours}
+            />
+        )
+    })
+
     return (
         <div className="PLACESList">
-            <PLACESSpace
-                name="Buckingham Fountain"
-                address="301 S Columbus Dr, Chicago, IL 60605"
-                image="buckingham-fountain.png"
-            />
-            <PLACESSpace
-                name="Chicago Riverwalk"
-                address="Chicago Riverwalk, Chicago, IL 60601"
-                image="chicago-riverwalk.jpeg"
-            />
-            <PLACESSpace
-                name="Lincoln Park Zoo"
-                address="2001 N Clark St, Chicago, IL 60614"
-                image="lincoln-zoo.jpeg"
-            />
-            <PLACESSpace
-                name="Metric Coffee Co."
-                address="2021 W Fulton St suite k-101b, Chicago, IL 60612"
-                image="metric-coffee.jpeg"
-            />
+            {places}
         </div>
     )
 }
--- src/PLACESSpace.js
+++ src/PLACESSpace.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg'
 
 function PLACESSpace(props) {
-    const { name, image, address } = props
-    return (
-        <div>
-            <img
-                src={`${process.env.PUBLIC_URL}/images/${image}`}
-                width="300"
-                height="300"
-                alt="Hello"
-            />
-            <h1>{name}</h1>
-            <div>{address}</div>
-        </div>
-    )
+  const { name, image, address, hours } = props;
+  return (
+    <div className="PLACESSpace">
+      <h1>{name}</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}/images/${image}`}
+        width="300"
+        height="300"
+        alt="Hello"
+      />
+      <div>{address}</div>
+      <div className="hours">{hours}</div>  
+    </div>
+  );
 }
 
 export default PLACESSpace
